0fb6d1253eb74b2497b7d9b164f358e2736f4830
Vladimir Fedorenko <vladimirfedorenko09720@gmail.com>
2023-07-27 22:01:25 +0300

    2dz

---
+++ script.js
@@ -0,0 +1,68 @@
+let e; //Задание 1
+
+e = 10;
+alert(e);
+
+e = 20;
+alert(e);
+
+let IPhone = 2007; //Задание 2
+
+alert(`Год выпуска первого IPhone ${IPhone}`);
+
+let name; //Задание 3
+
+name = 'Brandan Eich';
+
+alert(name)
+
+let f = 10; //Задание 4
+let c = 2;
+
+
+alert(f + c)
+alert(f - c)
+alert(f * c)
+alert(f / c)
+
+let d = 2; //Задание 5
+
+result = d ** 5;
+
+alert(result)
+
+let a = 9; //Задание 6
+let b = 2;
+
+alert(a % b)
+
+let num = 1; //Задание 7
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+let age = Number(prompt('Сколько вам лет?')) //Задание 8
+
+alert(`Вам ${age}`);
+
+const user = { //Задание 9
+    newname: 'Ell',
+    newage: 20,
+    isAdmin: true,
+}
+
+user['city of residence'] = true; //Задание 9.1
+user.newage = 22 //Задание 9.2
+delete user['city of residence']; //Задание 9.3
+let info = prompt("Какую информацию хотите узнать о пользователе?", "newname");//Задание 9.4
+alert(info);
+console.log(user[info]);
+
+let names = prompt('Как тебя зовут?') //Задание 10
+
+alert(`Привет, ${names}!`)
+
